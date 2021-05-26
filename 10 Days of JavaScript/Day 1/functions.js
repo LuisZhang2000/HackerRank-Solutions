186d2403f6a5f7638b32701bc0594217469f64ef
Luis Zhang <42627737+LuisZhang2000@users.noreply.github.com>
2021-05-26 11:08:31 +1000

    Create functions.js

---
+++ 10 Days of JavaScript/Day 1/functions.js
@@ -0,0 +1,18 @@
+/*
+ * Create the function factorial here
+ */
+
+function factorial(n) {
+    
+    if (n >= 1) 
+        return n * factorial(n - 1);
+    else 
+        return 1;      
+}
+
+
+function main() {
+    const n = +(readLine());
+    
+    console.log(factorial(n));
+}
